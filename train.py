69b2e08adafed726d7aabe3f2d9503a609fa6793
Shubh-Nil <chikushubhranil@gmail.com>
2025-04-14 11:19:44 +0530

    Built an overall framework for Band Selection by Genetic Algorithms

---
+++ train.py
@@ -0,0 +1,106 @@
+import numpy as np
+import scipy.io
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import f1_score
+
+from utils import *
+
+# individual = candidate = list of genes
+# band = gene
+# population = list of individuals
+# pool - population (initially, or modified)
+
+def genetic_algorithm():
+    """
+    Runs the Genetic Algorithm to select an optimal subset of spectral-bands.
+        - Population initialization
+        - Iteratively performs 'Crossover' and 'Mutation', to generate 'offspring'
+        - Evaluated fitness of all candidates (both parents and offsprings)
+        - Selects the top individuals for the next generation
+
+    Returns -> the Best Candidate (set of spectral-band indices)
+    """
+
+    n = 10                                                                                               # Number of genes per individual's chromosome.
+    ind = 8                                                                                              # Population size (Set a value, such that 'ind' and 'ind//2' are even)
+    generations = 3                                                                                      # Number of 'generations' to run.
+    
+    # Load and preprocess the HyperSpectral Image.
+    X, y = read_HSI()
+    X_flat, y_flat = flatten_data(X, y)                                                                  # X_flat.shape = (C, H*W) = (200, 21025)
+                                                                                                         # y_flat.shape = (H*W,) = (21025,)
+    
+    # Initialize the population.
+    population = initiate_population(ind, n)
+    print("Population initialized")
+    
+    # Run the genetic algorithm for a fixed number of generations.
+    for gen in range(generations):
+        print("\nGeneration:", gen)
+        print("Current Population:\n", population)
+
+        # 1.
+        # Evaluate fitness for the entire population
+        fitness_scores = fitness_all(population, X_flat, y_flat)
+        print("Fitness scores:", fitness_scores)
+
+        # Select the top 'ind//2' individuals based on fitness.
+        top_indices = np.argsort(fitness_scores)[::-1][:ind//2]                                          # [:] = full list
+                                                                                                         # [::-1] = full list in reverse (-1 means step backwards)
+                                                                                                         # select the first 'ind//2' candidate indices, from the reversed sorted list
+                                                                                                         # i.e. select the Top 'ind//2' candidate indices, with highest scores
+        parents = population[top_indices]
+        parent_fitness = fitness_scores[top_indices]
+        print("Selected parents:\n", parents)
+        
+        # Convert parent fitness scores to selection probabilities
+        fitness_sum = np.sum(parent_fitness)
+        if fitness_sum == 0:
+            parent_probs = np.full_like(parent_fitness, 1 / len(parent_fitness))                         # each parent having equal probabilities
+        else:
+            parent_probs = parent_fitness / fitness_sum
+        
+        offspring = []
+        # Generate Mutated offspring-chromosomes, from parent-chromosomes
+
+        # Standard selection
+        # for i in range(0, len(parents) - 1, 2):
+        #     parent1 = parents[i]
+        #     parent2 = parents[i + 1]
+        # 
+        #     off1, off2 = crossover_single_point(parent1, parent2)
+        #     offspring.append(mutate(off1))
+        #     offspring.append(mutate(off2))
+
+
+        # 2.
+        # Roulette-Wheel selection  (Fitness proportional selection)
+        while len(offspring) < (ind - len(parents)):
+            # Sample two parents (with replacement)
+            idx1, idx2 = np.random.choice(len(parents), size=2, replace=True, p=parent_probs)
+            parent1 = parents[idx1]
+            parent2 = parents[idx2]
+
+            # 3.
+            off1, off2 = crossover_single_point(parent1, parent2)
+            # 4.
+            offspring.append(mutate(off1))
+            offspring.append(mutate(off2))
+
+        
+        # Combine the current population with the new offspring.
+        combined_pool = np.vstack((population, np.array(offspring)))
+        print("Combined pool (Population + Offspring):\n", combined_pool)
+        
+        # New population = parents + offspring (N/2 + N/2 = N)
+        population = np.vstack((parents, np.array(offspring)))
+        print("New population:\n", population)
+        
+    # Output the final results.
+    print("\nFinal Population (Best Band Combinations):\n", population)
+    print("Best individual:", population[0])                                                        # final candidate with highest score
+
+
+if __name__ == '__main__':
+    genetic_algorithm()
---
+++ utils.py
@@ -0,0 +1,162 @@
+import numpy as np
+import scipy.io
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import f1_score
+
+def read_HSI() -> tuple[np.ndarray, np.ndarray]:
+    """
+    Reads HyperSpectral Image (HSI) data and the Ground-truth labels from MATLAB .mat files.
+    
+    Returns:
+        X : HSI with shape (H, W, C)
+        y : Ground-truth labels with shape (H, W)
+    """
+    X = scipy.io.loadmat('Indian_pines_corrected.mat')['indian_pines_corrected']
+    y = scipy.io.loadmat('Indian_pines_gt.mat')['indian_pines_gt']
+    return X, y
+
+def flatten_data(X: np.ndarray, y: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    """    
+    Each row corresponds to one spectral band (gene)  
+    Each column of the X.flatten represents a pixel of X.
+    
+    Args:
+        X : HSI with shape (H, W, C)
+        y : Ground-truth labels with shape (H, W)
+    
+    Returns:
+        X_flat : shape (C, H*W), where each row is a flattened band.
+        y_flat : shape (H*W,)
+    """
+    H, W, C = X.shape
+    X_flat = np.array([X[:, :, i].flatten() for i in range(C)])                         # Flatten each band (x[:,:,i]) into a 1D array, 
+                                                                                        # and stack into a 2D array with shape (C, H*W)
+    y_flat = y.flatten()
+    return X_flat, y_flat
+
+def initiate_population(ind: int, n: int) -> np.ndarray:
+    """
+    Initializes a Population for the genetic algorithm.
+    
+    Each individual is represented by a 'chromosome' containing n genes
+    (selected spectral-band indices), chosen randomly from the possible set of genes.
+    
+    Args:
+        n : Number of bands (genes) per individual.
+        ind : Population size (Number of individuals).
+        
+    Returns: 
+        population : array of shape (ind, n).
+    """
+    population = np.random.randint(0, 200, size=(ind, n))
+    return population
+
+
+
+def crossover_single_point(parent1: np.ndarray, parent2: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Performs a single-point crossover between two parent chromosomes.
+        - A random crossover point is chosen (excluding the endpoints) 
+        - The genes are swapped between the two parents after that point
+        - Create two offsprings.
+    """
+    # Choose a random crossover point (must be at least 1 and less than chromosome length)
+    point = np.random.randint(1, len(parent1))
+    print("Crossover point:", point)
+    offspring1 = np.concatenate((parent1[:point], parent2[point:]))
+    offspring2 = np.concatenate((parent2[:point], parent1[point:]))
+    
+    return offspring1, offspring2
+
+def crossover_double_point(parent1: np.ndarray, parent2: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Performs a double-point crossover between two parent chromosomes.
+        - Two random crossover points are chosen (point1 < point2)
+        - The segment between these points is swapped between the two parents
+        - Returns two new offspring
+    """
+
+    point1 = np.random.randint(1, len(parent1) - 1)
+    point2 = np.random.randint(point1 + 1, len(parent1))
+
+    print(f"Double-point crossover between {point1} and {point2}")
+
+    offspring1 = np.concatenate((parent1[:point1], parent2[point1:point2], parent1[point2:]))
+    offspring2 = np.concatenate((parent2[:point1], parent1[point1:point2], parent2[point2:]))
+
+    return offspring1, offspring2
+
+def crossover_uniform(parent1: np.ndarray, parent2: np.ndarray, p: float = 0.5) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Performs uniform crossover between two parents.
+        - For each gene, with probability `p`, genes are swapped.
+    """
+    mask = np.random.rand(len(parent1)) < p                                             # swap-decision mask (boolean)
+                                                                                        # 'len(parent1)' random numbers between 0 and 1
+                                                                                        # if they are < p, then mask[i] is 'True'. Else 'False'.
+    print("Uniform crossover mask:", mask)
+
+    offspring1 = parent1.copy()
+    offspring2 = parent2.copy()
+    # Apply mask-based swapping (in-place)
+    offspring1[mask], offspring2[mask] = parent2[mask], parent1[mask]                   # "swap" when mask[i] = True
+
+    return offspring1, offspring2
+
+
+
+def mutate(individual: np.ndarray) -> np.ndarray:
+    """
+    Mutates an individual chromosome, by randomly changing one gene to a new spectral band index.
+    """
+    mutated = individual.copy()
+    gene_index = np.random.randint(0, len(mutated))
+    mutated[gene_index] = np.random.randint(0, 200)
+    return mutated
+
+
+
+def fitness(candidate: np.ndarray, X_flat: np.ndarray, y_flat: np.ndarray) -> float:
+    """
+    Evaluates the fitness of a candidate chromosome (a set of spectral bands) using SVM classification.
+    
+    For each gene (spectral band index) in the candidate, the corresponding band data 
+    is extracted from the flattened HSI data, reshaped to a column vector, and used to 
+    train and test an SVM classifier. The weighted F1 score is computed, and the final 
+    fitness is the average F1 score over all genes.
+    
+    Args:
+        candidate (np.ndarray): Candidate chromosome containing spectral band indices.
+        X (np.ndarray): Flattened HSI data of shape (C, H*W) (each row corresponds to a band).
+        y (np.ndarray): Flattened ground truth array of shape (H*W,).
+        
+    Returns:
+        float: The average weighted F1 score across the bands in the candidate.
+    """
+    scores = []                                                                                                      # F1 scores for each band (gene)
+    for band in candidate:                                                                                           # n bands <= total bands of HSI
+        band = int(band)
+        # Extract the data for this band and reshape to a column vector
+        x_data = X_flat[band].reshape(-1, 1)                                                                         # x_data -> band'th feature map of the HSI, as a column-vector                                      
+                                                                                                                     # x_data.shape = (H*W, 1)
+        X_train, X_test, y_train, y_test = train_test_split(x_data, y_flat, test_size=0.20, random_state=42)         # y.flat = (H*W,)
+        
+        clf = svm.SVC(decision_function_shape='ovo', probability=True)
+        clf.fit(X_train, y_train)                                                                                    # everytime SVM classifier is trained from scratch, for each of the n bands (genes)
+                                                                                                                     # for each of the 'ind' candidate chromosomes
+                                                                                                                     # across 'gen' generations
+        y_pred = clf.predict(X_test)
+
+        score = f1_score(y_test, y_pred, average='weighted')
+        scores.append(score)
+    avg_score = np.mean(scores)
+    print("Score list:", scores)
+    print("Average Score:", avg_score)
+    return avg_score
+
+def fitness_all(pool: np.ndarray, X_flat: np.ndarray, y_flat: np.ndarray) -> list:
+    """
+    Evaluates the fitness scores for each candidate chromosome, in the entire population.
+    """
+    return [fitness(candidate, X_flat, y_flat) for candidate in pool]
